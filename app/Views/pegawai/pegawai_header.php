f5fbde33fab5abd5b26a24bc9d2d88b733a495de
qoharul <qoharuljabbar@gmail.com>
2021-11-17 23:05:34 +0700

    Update pegawai_header.php

--- app/Views/pegawai/pegawai_header.php
+++ app/Views/pegawai/pegawai_header.php
@@ -47,7 +47,7 @@
   </div>
 
   <!-- Navbar -->
-  <nav class="main-header navbar navbar-expand navbar-success navbar-light">
+  <nav class="main-header navbar navbar-expand navbar-white navbar-light">
     <!-- Left navbar links -->
     <ul class="navbar-nav">
       <li class="nav-item">
